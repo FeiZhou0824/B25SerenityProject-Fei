e0f53bf7788d16ea446fe32ae1ab8846ca7ff6a2
FeiZhou <sophia08241@gmail.com>
2022-05-15 01:21:14 -0400

    Spartan Admin Test added

--- src/test/java/b25/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/b25/spartan/admin/SpartanAdminGetTest.java
@@ -1,4 +1,36 @@
 package b25.spartan.admin;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static net.serenitybdd.rest.SerenityRest.given;
+import static net.serenitybdd.rest.SerenityRest.lastResponse;
+import static org.hamcrest.Matchers.*;
+
+
 public class SpartanAdminGetTest {
+
+
+    @BeforeAll
+    public static void init(){
+        RestAssured.baseURI ="http://44.201.121.105:7000";
+    }
+
+
+    @Test
+    public void getAllSpartan(){
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .when()
+                .get("/api/spartans")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON);
+    }
 }
