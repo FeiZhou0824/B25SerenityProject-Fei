f3a9bbfe4714bb4d202ede20352a026b51f8d52a
Fei.Zhou <sophia08241@gmail.com>
2022-05-16 16:19:24 -0400

    Serenity

--- src/test/java/b25/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/b25/spartan/admin/SpartanAdminGetTest.java
@@ -1,19 +1,25 @@
 package b25.spartan.admin;
 
+import io.cucumber.java.af.En;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.Ensure;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
 
 import static net.serenitybdd.rest.SerenityRest.given;
 import static net.serenitybdd.rest.SerenityRest.lastResponse;
 import static org.hamcrest.Matchers.*;
 
 
+
+@SerenityTest
 public class SpartanAdminGetTest {
 
-
+    //beforeAll is the same thing with beforeClass in testng
     @BeforeAll
     public static void init(){
         RestAssured.baseURI ="http://44.201.121.105:7000";
@@ -32,5 +38,37 @@ public class SpartanAdminGetTest {
                 .statusCode(200)
                 .and()
                 .contentType(ContentType.JSON);
+    }
+
+
+    @Test
+    public void getOneSpartan(){
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .pathParam("id",8)
+                .when()
+                .get("/api/spartans/{id}");
+
+        //if you send a request using serenityRest, the response object
+        //can be obtained from the method called lastResponse() without being saved separately
+        //same with Response response object
+
+        System.out.println("lastResponse().statusCode() = " + lastResponse().statusCode());
+
+        System.out.println("lastResponse().path(\"id\") = " + lastResponse().path("id"));
+
+        System.out.println("lastResponse().jsonPath().getString(\"name\") = " + lastResponse().jsonPath().getString("name"));
+
+        Ensure.that("Status code is 200", vRes -> vRes.statusCode(200));
+
+        Ensure.that("Content-type is JSON", vRes -> vRes.contentType(ContentType.JSON));
+
+        Ensure.that("ID is 8", vRes -> vRes.body("id",is(8)));
+
+
+
     }
 }
