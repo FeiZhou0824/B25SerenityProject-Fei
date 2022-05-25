d2f89d1eeca9e166206cd00764dd957eaad4d4a3
Fei.Zhou <sophia08241@gmail.com>
2022-05-25 10:54:19 -0400

    Serenity

--- src/test/java/b25/spartan/editor/SpartanEditorPostTest.java
+++ src/test/java/b25/spartan/editor/SpartanEditorPostTest.java
@@ -2,6 +2,9 @@ package b25.spartan.editor;
 
 import io.cucumber.java.af.En;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import utilities.SpartanNewBase;
 
 
@@ -16,15 +19,17 @@ import org.junit.jupiter.api.Test;
 import utilities.SpartanUtil;
 
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static net.serenitybdd.rest.SerenityRest.given;
 import static net.serenitybdd.rest.SerenityRest.lastResponse;
 import static org.hamcrest.Matchers.*;
-@Disabled
+
 @SerenityTest
 public class SpartanEditorPostTest extends SpartanNewBase {
 
+    @Disabled
     @DisplayName("Editor should be able to POST")
     @Test
     public void postSpartanAsEditor(){
@@ -85,8 +90,49 @@ public class SpartanEditorPostTest extends SpartanNewBase {
 
 
 
+    }
+
+    /*
+    so if we want to provide custom test name for each execution
+    we can use name  = "some message" structure. if we want to include index
+    we will use {index}and for using parameter values we use order of parameter index
+    just like {0} - name {1} - gender{2} - phone.
+     */
+
+    @ParameterizedTest(name = "New Spartan {index} - name - {0}")
+    @CsvFileSource(resources = "/SpartanData.csv",numLinesToSkip = 1)
+    public void postSpartanWithCsvFile(String nameArg, String gender,long phone){
+
+        System.out.println("nameArg = " + nameArg);
+        System.out.println("gender = " + gender);
+        System.out.println("phone = " + phone);
+        Map<String,Object> spartanMap = new LinkedHashMap<>();
+        spartanMap.put("name",nameArg);
+        spartanMap.put("gender",gender);
+        spartanMap.put("phone",phone);
 
 
+        //send a post REQUEST AS EDITOR
+        given()
+                .auth().basic("editor","editor")
+                .and()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body(spartanMap)
+                .log().body()
+                .when()
+                .post("/spartans")
+                .then().log().all();
+
+
+        //status code is 201
+        Ensure.that("Status code is 201",vRes ->vRes.statusCode(201));
+
+        //content type is Json
+        Ensure.that("Content -Type is JSON", vR -> vR.contentType(ContentType.JSON));
+
+        // success message is A Spartan is Born!
+        Ensure.that("success message is A Spartan is Born!", vR-> vR.body("success", is("A Spartan is Born!")));
 
 
     }
